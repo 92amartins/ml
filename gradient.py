85dfa27a021325683588905a5e16ce51b5e4d08f
Andrei Martins <amartins13@outlook.com>
2016-09-01 11:44:09 -0300

    Plot feature added

--- gradient.py
+++ gradient.py
@@ -2,31 +2,54 @@ import numpy as np
 from numpy import linalg as la
 import matplotlib.pyplot as plt
 
+'''Gradient descent for 2-dimensions minimization'''
 class Gradient():
-
-    def __init__(self, alpha = 0.25, precision = 10**-6):
-        self.prec = precision
-        self.alpha = alpha
-        
-    def descent(self):
-        x = [0.0, 0.0]
-        Xs = list()
-
-        while True:
-            d = np.negative(self.gradf(x))
-            if(la.norm(d) <= self.prec):
-                break
-            x = x + self.alpha * d
-
-        print(x)
-        return x
-
     
-    def gradf(self, X):
-        return [4*(X[0] - 2),
-                4*(X[1] - 3)]
-                         
-  
+    def __init__(self, alpha = 0.01, precision = 10**-6, X = np.array([[0.] , [0.]])):
+         self.Xs = [] # Candidates
+         self.F = [] # Evaluations
+         self.Xs.append(X) # Append first candidate (X0)
+         self.alpha = alpha   # Learning rate
+         self.precision = precision # Stop criteria
+
+    '''Returns the gradient of f(x1, x2) evaluated at X'''
+    def gradient(self, X):
+        x1 = X[0][0]
+        x2 = X[1][0]
+        
+        return np.array([[4*(x1 - 2)],
+                         [4*(x2 - 3)]])
+
+    '''Evaluate f at X'''
     def evaluate(self, X):
-        return 2*(X[0] - 2)**2 + 2*(X[1] - 3)**2
+        return 2*(X[0] - 2)**2 + 2*(X[1] - 3)**2 # Function to be minimized
+
+    '''Returns the point (x1, x2) which minimizes f'''
+    def descent(self):
+        d = - self.gradient(self.Xs[0])
+
+        # Calculate new X and Update direction
+        while la.norm(d) > self.precision:
+            X = self.Xs[-1] + self.alpha*d
+            self.Xs.append(X)
+            d = - self.gradient(self.Xs[-1])
+
+        # PLOTTING
+        x1s = []
+        x2s = []
+        for X in self.Xs:
+            x1s.append(X[0][0])
+            x2s.append(X[1][0])
+        print('mock')
+        print('x1 =', x1s)
+        print('x2 =', x2s)
+
+        plt.plot(x1s, x2s, 'ro')
+        plt.xlabel('X1')
+        plt.ylabel('X2')
+        plt.show()
+        
+        return self.Xs[-1]
+
+        
          
--- test.py
+++ test.py
@@ -1,14 +1,37 @@
 import unittest
-import gradient as g
 import numpy as np
+import gradient as g
+import conjugate as c
+import matplotlib.pyplot as plt
 
 class TestGradient(unittest.TestCase):
 
     def setUp(self):
-        self.grad = g.Gradient(0.25)
+        self.grad = g.Gradient(0.4)
+        self.conj = c.Conjugate(0.4)
 
-    def testDescent(self):
-        self.assertEqual(list(self.grad.descent()), [2.0, 3.0])
+    def testGradient(self):
+        X = np.array([[0.0],[0.0]])
+        x1 = X[0][0]
+        x2 = X[1][0]
+        expected = np.array([[4*(x1-2)], [4*(x2-3)]])
+        result = self.conj.gradient(X)
+        self.assertTrue(np.allclose(expected, result))
+        
+    def testConjugate(self):
+        expected = np.array([[2.0], [3.0]])
+        result = self.conj.descent()
+        self.assertTrue(np.allclose(expected, result))
+
+    def testGradientDescent(self):
+        expected = np.array([[2.0], [3.0]])
+        result = self.grad.descent()
+        self.assertTrue(np.allclose(expected, result))
+
+        
         
 if __name__ == '__main__':
     unittest.main()
+
+
+ 
