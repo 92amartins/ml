f2177bc2d9d611ed763922eace517193426e63a6
Andrei Martins <amartins13@outlook.com>
2016-09-03 18:27:01 -0300

    Updates test suite

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
 import unittest
 import numpy as np
+import bisection as bis
 import gradient as g
 import conjugate as c
+import levenberg as l
+import newton as n
 import matplotlib.pyplot as plt
 
 class TestGradient(unittest.TestCase):
 
     def setUp(self):
-        self.grad = g.Gradient(0.01)
+        self.grad = g.Gradient()
         self.conj = c.Conjugate()
+        self.leven = l.Levenberg()
+        self.newton = n.Newton()
 
     def testGradientDescent(self):
         expected = np.array([[2.0], [3.0]])
@@ -22,6 +27,18 @@ class TestGradient(unittest.TestCase):
         self.conj.plot('Gradiente Conjugado', 'X1', 'X2')
         self.assertTrue(np.allclose(expected, result))
 
+    def testLevenberg(self):
+        expected = np.array([[2.0], [3.0]])
+        result = self.leven.descent()
+        self.leven.plot('Levenberg-Marquardt', 'X1', 'X2')
+        self.assertTrue(np.allclose(expected, result))
+
+    def testNewton(self):
+        expected = np.array([[2.0], [3.0]])
+        result = self.newton.descent()
+        self.newton.plot('Newton Modificado', 'X1', 'X2')
+        self.assertTrue(np.allclose(expected, result))
+        
 
     def testGradient(self):
         X = np.array([[0.0],[0.0]])
@@ -36,6 +53,13 @@ class TestGradient(unittest.TestCase):
         x2 = X[1][0]
         expected = np.array([[4*(x1-2)], [4*(x2-3)]])
         result = self.grad.gradient(X)
+        self.assertTrue(np.allclose(expected, result))
+
+        X = np.array([[0.0],[0.0]])
+        x1 = X[0][0]
+        x2 = X[1][0]
+        expected = np.array([[4*(x1-2)], [4*(x2-3)]])
+        result = self.newton.gradF(X)
         self.assertTrue(np.allclose(expected, result))
 
     def testHessiana(self):
@@ -64,10 +88,19 @@ class TestGradient(unittest.TestCase):
 
         self.assertEqual(expected, result)
 
-    
+    def testBuildGradientR(self):
+        expected = np.array([[4, 0], [0, 4]])
+        result = self.leven.buildGradientR()
+        
+        self.assertTrue(np.allclose(expected, result))
 
-    
-
+    def testBisection(self):
+        expected = 0.25
+        X = np.array([[0.0],[0.0]])
+        d = np.array([[8], [12]])
+        result = bis.getAlpha(X, d)
+        self.assertEqual(expected, result)
+        
     
         
         
